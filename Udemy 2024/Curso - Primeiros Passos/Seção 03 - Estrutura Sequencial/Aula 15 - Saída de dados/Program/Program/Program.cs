9e47c53075750f1dadd55f4bde4c8dc193e9d676
PBPaschoal <programming.paschoal@gmail.com>
2024-02-17 18:42:31 -0300

    Entrada e saida de dados

---
+++ Udemy 2024/Curso - Primeiros Passos/Seção 03 - Estrutura Sequencial/Aula 15 - Saída de dados/Program/Program/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá mundo");
+            Console.Write("Bom dia!");
+
+            double x = 10.35784;
+
+            Console.WriteLine(x);
+
+            Console.WriteLine(x.ToString("F2"));
+            Console.WriteLine(x.ToString("F4")); //Vai imprimir com virgula ao inves de ponto
+
+            Console.WriteLine(x.ToString("F2", CultureInfo.InvariantCulture)); //Vai imprimir com ponto ao inves de virgula
+
+            Console.WriteLine("RESULTADO = " + x);
+
+            Console.WriteLine("O valor do troco é " + x + " reais");
+
+            Console.WriteLine("O valor do troco é " + x.ToString("F2") + " reais");
+        }
+    }
+}
