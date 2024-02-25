61e6f9065b6f847d56c38c53ee4a3dbb8a7182e8
PBPaschoal <programming.paschoal@gmail.com>
2024-02-25 19:51:01 -0300

    Aula 18, continuação sobre entrada de dados

---
+++ Udemy 2024/Curso - Primeiros Passos/Seção 03 - Estrutura Sequencial/Aula 18 - Entrada de dados Parte 02/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            char ch = char.Parse(Console.ReadLine());
+            double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            string[] vet = Console.ReadLine().Split(' ');
+            string nome = vet[0];
+            char sexo = char.Parse(vet[1]);
+            int idade = int.Parse(vet[2]);
+            double altura = 
+
+
+            Console.WriteLine("Você digitou: " + n1);
+            Console.WriteLine("Você digitou: " + ch);
+            Console.WriteLine("Você digitou: " + n2.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
