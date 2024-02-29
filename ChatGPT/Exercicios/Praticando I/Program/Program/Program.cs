21df0e1707e25e97dd9913c9c80a835da0783bd3
PBPaschoal <programming.paschoal@gmail.com>
2024-02-28 22:16:33 -0300

    Praticando exercicios do ChatGPT

--- ChatGPT/Exercicios/Praticando I/Program/Program/Program.cs
+++ ChatGPT/Exercicios/Praticando I/Program/Program/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System.Globalization;
 
 int numero, soma = 0;
+int contaemRegressiva;
 
-Console.WriteLine("Quantas vezes deseja repetir?");
+Console.Write("Quantas vezes deseja repetir? ");
 byte repetir = byte.Parse(Console.ReadLine());
 
 for (int i = 0; i < repetir; i++) {
-    Console.WriteLine("Digite um numero inteiro positivo: ");
+    Console.Write("Digite um numero inteiro positivo: ");
     numero = int.Parse(Console.ReadLine());
     soma += numero;
 }
@@ -19,9 +20,9 @@ if (soma % 2 == 0) {
     Console.WriteLine(soma + " é um número ímpar.");
 }
 
-Console.WriteLine("\nInforme a base do triangulo: ");
+Console.Write("\nInforme a base do triangulo: ");
 double baseTriangulo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-Console.WriteLine("Informe a altura do triangulo: ");
+Console.Write("Informe a altura do triangulo: ");
 double alturaTriangulo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 double areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
 Console.WriteLine("A area do triangulo é = " + areaTriangulo.ToString("F2", CultureInfo.InvariantCulture));
@@ -40,4 +41,12 @@ if (textoReverso == textoOriginal) {
     Console.WriteLine("É um Palíndromo.");
 } else {
     Console.WriteLine("Não é um Palíndromo.");
+}
+
+Console.WriteLine();
+Console.Write("Informe um número inteiro positivo: ");
+contaemRegressiva = int.Parse(Console.ReadLine());
+
+for (int i = contaemRegressiva; i >= 0; i--) {
+    Console.WriteLine(i);
 }
