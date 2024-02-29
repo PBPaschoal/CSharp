3a0ac221ad9a57f84e10b8ff3becf9deeb4bdedc
PBPaschoal <programming.paschoal@gmail.com>
2024-02-29 19:36:15 -0300

    Praticando 10 exercicios básicos desafiados pelo ChatGPT - Nv 1

--- ChatGPT/Exercicios/Praticando I/Program/Program/obj/Debug/net8.0/Program.AssemblyInfo.cs
+++ ChatGPT/Exercicios/Praticando I/Program/Program/obj/Debug/net8.0/Program.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Program")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21df0e1707e25e97dd9913c9c80a835da0783bd3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Program")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Program")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
