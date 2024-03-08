9f20a706a4bc3c80c397a28b56c1608c2daffd34
PBPaschoal <programming.paschoal@gmail.com>
2024-03-07 23:04:53 -0300

    Exercicio 13

---
+++ ChatGPT/Exercicios/Praticando II/Exercicio013/Exercicio013/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Exercício 13: Contagem de Números Pares
+ * Crie um programa que solicite ao usuário inserir um número e, em seguida, conte e exiba a quantidade de 
+ * números pares de 1 até o número inserido. */
+
+using System;
+
+namespace Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exercício 11: Contagem de Números Pares");
+            Console.Write("Informe um número: ");
+            int numero = int.Parse(Console.ReadLine());
+            int par = 0;
+
+            for (int i = 0; i <= numero; i++) {
+                if (i % 2 == 0) {
+                        par++;
+                }
+            }
+
+            Console.WriteLine("Quantidade de numeros pares, total = " + par);
+
+        }
+    }
+}
