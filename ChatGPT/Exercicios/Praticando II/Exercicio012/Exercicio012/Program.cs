c283bad35db89dab3812daa3a16c4fa386798ca4
PBPaschoal <programming.paschoal@gmail.com>
2024-03-09 18:56:15 -0300

    Exercício 13: Multiplos de 3 e 5 Crie um programa que exiba a soma de todos os múltiplos de 3 ou 5 até um número fornecido pelo usuário

---
+++ ChatGPT/Exercicios/Praticando II/Exercicio012/Exercicio012/Program.cs
@@ -0,0 +1,29 @@
+﻿/* Exercício 12: Soma de Números Ímpares
+ * Desenvolva um programa que solicite ao usuário inserir um número e calcule a soma dos números ímpares de 1 até o número inserido. */
+
+using System;
+
+namespace Program {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Exercício 12 - Soma de números ímpares");
+            Console.Write("Informe um número inteiro positivo: ");
+            int numero = int.Parse(Console.ReadLine());
+            int impar = 0;
+            int soma = 0;
+
+            for (int i = 1; i <= numero; i++) { 
+                if(i % 2 != 0) {
+                    impar++;
+                    soma += i;
+                }
+            }
+
+            if (soma >= 1) { 
+                Console.WriteLine("O total da soma dos numeros impares = " + soma);
+            } else {
+                Console.WriteLine("O número precisa ser inteiro e positivo!");
+            }
+        }
+    }
+}
---
+++ ChatGPT/Exercicios/Praticando II/Exercicio013/Exercicio013/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Exercício 13: Multiplos de 3 e 5
+ * Crie um programa que exiba a soma de todos os múltiplos de 3 ou 5 até um número fornecido pelo usuário. */
+
+using System;
+
+namespace Program { 
+    internal class Program { 
+    
+        static void Main(string[] args) {
+
+            Console.WriteLine("Exercício 013");
+            Console.Write("Informe um número inteiro e positivo: ");
+            int numero = int.Parse(Console.ReadLine());
+            int soma = 0;
+
+            for(int i = 1; i <= numero; i++) {
+                if (i % 3 == 0 || i % 5 == 0) {
+                    soma += i;
+                }
+            }
+
+            if (numero >= 3) {
+                Console.WriteLine("A soma dos multipos de 3 ou 5 até " + numero + " é: " + soma);
+            } else {
+                Console.WriteLine("Você precisa informar um número inteiro positivo igual ou acima de 3 para calcular a soma dos múltiplos de 3 ou 5.");
+            }
+        }
+    }
+
+}
