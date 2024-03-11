8d805c2944d3b6ea07bb459d85a5c5ab6bc67af4
PBPaschoal <programming.paschoal@gmail.com>
2024-03-11 20:44:48 -0300

    Exercício 1004 beecrowd

--- Udemy 2024/Curso - Primeiros Passos/Seção 03 - Estrutura Sequencial/Aula 23 - Exercícios Propostos PARTE I/E1003beecrowd/E1003beecrowd/Program.cs
+++ Udemy 2024/Curso - Primeiros Passos/Seção 03 - Estrutura Sequencial/Aula 23 - Exercícios Propostos PARTE I/E1003beecrowd/E1003beecrowd/Program.cs
@@ -1,4 +1,5 @@
-﻿/* Leia dois valores inteiros, no caso para variáveis A e B. A seguir, calcule a soma entre elas e atribua à variável SOMA. 
+﻿/* Soma Simples
+ * Leia dois valores inteiros, no caso para variáveis A e B. A seguir, calcule a soma entre elas e atribua à variável SOMA. 
  * A seguir escrever o valor desta variável.
  * 
  * Entrada
