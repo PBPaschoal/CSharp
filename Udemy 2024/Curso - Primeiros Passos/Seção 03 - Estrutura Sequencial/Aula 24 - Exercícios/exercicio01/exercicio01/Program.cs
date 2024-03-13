6b8d10c6050761c774dfb6eb462da1c046c1c555
PBPaschoal <programming.paschoal@gmail.com>
2024-03-13 20:35:39 -0300

    Aula 24, exercício.

---
+++ Udemy 2024/Curso - Primeiros Passos/Seção 03 - Estrutura Sequencial/Aula 24 - Exercícios/exercicio01/exercicio01/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Fazer um programa para ler as medidas da base e altura de um retângulo. Em seguida, mostrar o valor da área, perímetro e diagonal deste 
+ * retângulo, com quatro casas decimais, conforme exemplo.
+ * Exemplo:
+ * 
+ * Entrada:             Saída:
+ * 5.0                  AREA = 20.0000
+ * 4.0                  PERIMETRO = 18.0000
+                        DIAGONAL = 6.4031 */
+
+using System;
+using System.Globalization;
+
+namespace Program {
+    internal class  Program { 
+        static void Main(string[] args) {
+
+            double medidaBase, medidaAltura, area, perimetro,diagonal;
+            medidaBase = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            medidaAltura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            area = medidaBase * medidaAltura;
+            perimetro = 2 * (medidaBase + medidaAltura);
+            diagonal = Math.Sqrt(Math.Pow(medidaBase, 2.0) + Math.Pow(medidaAltura, 2.0));
+            Console.WriteLine("AREA = " + area.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("PERIMETRO = " + perimetro.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("DIAGONAL = " + diagonal.ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+}
