4794bff785f4bc14bfbd78cf58fc55e75eb43d44
PBPaschoal <programming.paschoal@gmail.com>
2024-03-23 23:20:47 -0300

    Estruturas condicionais

---
+++ Udemy 2024/Curso - Primeiros Passos/Seção 04 - Estrutura condicional/Aula 32 - Estrutura condicional/Exemplo/Exemplo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Program { 
+    internal class Program {
+        static void Main(string[] args) {
+
+            int x = 5;
+            int horas, horas2;
+
+            Console.WriteLine("Teste 01");
+            Console.WriteLine("Bom dia!");
+            Console.WriteLine("Boa tarde!");
+            Console.WriteLine("Boa noite!");
+            Console.WriteLine();
+
+
+            Console.WriteLine("Teste 02");
+            Console.WriteLine("Bom dia!");
+
+            if(x < 0) { 
+                Console.WriteLine("Boa tarde!");
+            }
+
+            Console.WriteLine("Boa noite!");
+
+            Console.WriteLine("Teste 03");
+            Console.WriteLine("OBS: Só bom dia e boa tarde");
+            horas = int.Parse(Console.ReadLine());
+            if (horas < 12) {
+                Console.WriteLine("Bom dia!");
+            } else {
+                Console.WriteLine("Boa tarde!");
+            }
+
+            Console.WriteLine("Teste 04");
+            horas2 = int.Parse(Console.ReadLine());
+            if(horas2 >= 0 && horas2 < 12) {
+                Console.WriteLine("Bom dia!");
+            } else if (horas2 < 18) {
+                Console.WriteLine("Boa tarde!");
+            } else {
+                Console.WriteLine("Boa noite!");
+            }
+        }
+    }
+}
