1dfd548b390dec78fe123be68803fd1d3c04eb74
PBPaschoal <programming.paschoal@gmail.com>
2024-03-25 21:51:49 -0300

    Mais exercícios

---
+++ Udemy 2024/Curso - Primeiros Passos/Seção 04 - Estrutura condicional/Aula 34 - Exercício Beecrowd/E1036beecrowd/E1036beecrowd/Program.cs
@@ -0,0 +1,50 @@
+﻿/* Aula 34 Exercício - 1036 Fórmula de Bhaskara - Beecrowd
+ * Leia 3 valores de ponto flutuante e efetue o cálculo das raízes da equação de Bhaskara. 
+ * Se não for possível calcular as raízes, mostre a mensagem correspondente “Impossivel calcular”, 
+ * caso haja uma divisão por 0 ou raiz de numero negativo.
+
+Entrada
+Leia três valores de ponto flutuante (double) A, B e C.
+
+Saída
+Se não houver possibilidade de calcular as raízes, apresente a mensagem "Impossivel calcular". 
+Caso contrário, imprima o resultado das raízes com 5 dígitos após o ponto, com uma mensagem correspondente 
+conforme exemplo abaixo. Imprima sempre o final de linha após cada mensagem.
+
+Exemplos de Entrada	                Exemplos de Saída
+10.0 20.1 5.1                       R1 = -0.29788
+                                    R2 = -1.71212
+
+0.0 20.0 5.0                        Impossivel calcular
+
+10.3 203.0 5.0                      R1 = -0.02466
+                                    R2 = -19.68408
+
+10.0 3.0 5.0                        Impossivel calcular */
+
+using System;
+using System.Globalization;
+
+namespace Program { 
+    internal class Program { 
+        static void Main(string[] args) {
+            double A, B, C, DELTA, R1, R2;
+
+            string[] vetor = Console.ReadLine().Split(' ');
+
+            A = double.Parse(vetor[0], CultureInfo.InvariantCulture);
+            B = double.Parse(vetor[1], CultureInfo.InvariantCulture);
+            C = double.Parse(vetor[2], CultureInfo.InvariantCulture);
+            DELTA = Math.Pow(B, 2.0) - 4 * A * C;
+
+            if(A == 0 || DELTA < 0.0) {
+                Console.WriteLine("Impossivel calcular");
+            } else {
+                R1 = (-B + Math.Sqrt(DELTA)) / (2.0 * A);
+                R2 = (-B - Math.Sqrt(DELTA)) / (2.0 * A);
+                Console.WriteLine("R1 = " + R1.ToString("F5", CultureInfo.InvariantCulture));
+                Console.WriteLine("R2 = " + R2.ToString("F5", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
---
+++ Udemy 2024/Curso - Primeiros Passos/Seção 04 - Estrutura condicional/Aula 35 - Exercício/Exercicio/Exercicio/Program.cs
@@ -0,0 +1,38 @@
+﻿/* Aula 35 - Exercício 
+ * Fazer um programa para ler três números inteiros.
+ * Em seguida, mostrar qual o menor dentro os três números lidos.
+ * Em caso de empate, mostrar apenas uma vez.
+ * 
+ * Exemplos:
+ * Entrada:         Saída:
+ * 7 3 8            MENOR = 3
+ * 
+ * Entrada:         Saída:
+ * 5 12 5           MENOR = 5
+ * 
+ * Entrada: 	    Saída:
+ * 9 9 9            MENOR = 9 */
+
+using System;
+
+namespace Program { 
+    internal class Program { 
+        static void Main(string[] args) {
+            int numero1, numero2, numero3;
+
+            string[] vetor = Console.ReadLine().Split(' ');
+
+            numero1 = int.Parse(vetor[0]);
+            numero2 = int.Parse(vetor[1]);
+            numero3 = int.Parse(vetor[2]);
+
+            if (numero1 < numero2 && numero1 < numero3) {
+                Console.WriteLine("MENOR = " + numero1);
+            } else if (numero2 < numero3) {
+                Console.WriteLine("MENOR = " + numero2);
+            } else {
+                Console.WriteLine("MENOR = " + numero3);
+            }
+        }
+    }
+}
