f755d3c5c1ae0b9ead4ada81cd96148b82b3bc31
PBPaschoal <programming.paschoal@gmail.com>
2024-03-26 21:44:15 -0300

    Aulas: swtich-case e exercício

---
+++ Udemy 2024/Curso - Primeiros Passos/Seção 04 - Estrutura condicional/Aula 36 - Operadores de atribuição cumulativa/Exercicio01/Exercicio01/Program.cs
@@ -0,0 +1,32 @@
+﻿/* Aula 36 Exercício 
+ * Uma operadora de telefonia cobra R$ 50.00 por um plano básico que dá direito a 100 minutos de telefone.
+ * Cada minuto que exceder a franquia de 100 minutos custa R$ 2.00.
+ * Fazer um programa para ler a quantidade de minutos que uma pessoa consumiu, daí mostrar o valor a ser pago.
+ * 
+ * Entrada: 		Saída:
+ * 22			    Valor a pagar: R$ 50.00
+ * 
+ * Entrada: 		Saída:
+ * 103			    Valor a pagar: R$ 56.00 */
+
+using System;
+using System.Globalization;
+
+namespace Program { 
+    internal class Program { 
+        static void Main(string[] args) {
+
+            int minutos;
+
+            minutos = int.Parse(Console.ReadLine());
+
+            if (minutos <= 100) {
+                Console.WriteLine("Valor a pagar: R$ 50.00");
+            } else {
+                double conta = 50.0;
+                conta += (minutos - 100) * 2.00;
+                Console.WriteLine("Valor a pagar: R$ " + conta.ToString("F2", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
---
+++ Udemy 2024/Curso - Primeiros Passos/Seção 04 - Estrutura condicional/Aula 37 - Sintaxe opcional estrutura switch-case/Exercicio/Exercicio/Program.cs
@@ -0,0 +1,56 @@
+﻿/* Fazer um programa para ler um valor inteiro de 1 a 7 representando um dia da semana
+ * (sendo 1=domingo, 2=segunda, e assim por diante).
+ * Escrever na tela o dia da semana correspondente, conforme exemplos:
+ * 
+ * Entrada: 			Saída:
+ * 1     				Dia da semana: domingo
+ * 
+ * Entrada: 			Saída:
+ * 4 		        	Dia da semana: quarta
+ * 
+ * Entrada:			    Saída:
+ * 9				    Dia da semana: valor inválido */
+
+using System;
+
+namespace Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor;
+            valor = int.Parse(Console.ReadLine());
+            string dia;
+
+            switch (valor)
+            {
+                case 1:
+                    dia = "domingo";
+                    break;
+                case 2:
+                    dia = "segunda";
+                    break;
+                case 3:
+                    dia = "terça";
+                    break;
+                case 4:
+                    dia = "quarta";
+                    break;
+                case 5:
+                    dia = "quinta";
+                    break;
+                case 6:
+                    dia = "sexta";
+                    break;
+                case 7:
+                    dia = "sábado";
+                    break;
+                default:
+                    dia = "valor inválido";
+                    break;
+            }
+            Console.WriteLine("Dia da semana: " + dia);
+        }
+    }
+}
