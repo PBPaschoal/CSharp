10ad3970ea7734b74df49910ab75c37077d95a30
PBPaschoal <programming.paschoal@gmail.com>
2024-04-02 21:52:08 -0300

    Exercicios

---
+++ Udemy 2024/Curso - Primeiros Passos/Seção 04 - Estrutura condicional/Aula 38 - Exercícios propostos/Visual Studio 2022/E1042beecrowd/E1042beecrowd/Program.cs
@@ -0,0 +1,77 @@
+﻿/* Aula 38 Exercício 1042 Beecrowd - Sort Simples
+ * 
+ * Leia 3 valores inteiros e ordene-os em ordem crescente. No final, mostre os valores em ordem crescente, 
+ * uma linha em branco e em seguida, os valores na sequência como foram lidos.
+ * 
+ * Entrada
+ * A entrada contem três números inteiros.
+ * 
+ * Saída
+ * Imprima a saída conforme foi especificado.
+ * 
+ * Exemplo de Entrada			Exemplo de Saída
+ * 7 21 -14				        -14
+ * 					              7
+ * 					             21
+ * 
+ *  				              7
+ * 					             21
+ * 					            -14
+ * 
+ * -14 21 7				        -14
+ * 					              7
+ * 					             21
+ * 
+ * 					            -14
+ * 					             21
+ * 					              7 */
+
+using System;
+
+namespace Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int value1, value2, value3;
+            string[] values = Console.ReadLine().Split(' ');
+
+            // Convertendo os valores para inteiros
+            value1 = int.Parse(values[0]);
+            value2 = int.Parse(values[1]);
+            value3 = int.Parse(values[2]);
+
+            // Ordenando os valores
+            if (value1 > value2)
+            {
+                int temp = value1;
+                value1 = value2;
+                value2 = temp;
+            }
+            if (value2 > value3)
+            {
+                int temp = value2;
+                value2 = value3;
+                value3 = temp;
+            }
+            if (value1 > value2)
+            {
+                int temp = value1;
+                value1 = value2;
+                value2 = temp;
+            }
+
+            // Exibindo os valores ordenados
+            Console.WriteLine(value1);
+            Console.WriteLine(value2);
+            Console.WriteLine(value3);
+
+            // Exibindo os valores na sequência original
+            Console.WriteLine();
+            Console.WriteLine(values[0]);
+            Console.WriteLine(values[1]);
+            Console.WriteLine(values[2]);
+        }
+    }
+}
---
+++ Udemy 2024/Curso - Primeiros Passos/Seção 04 - Estrutura condicional/Aula 38 - Exercícios propostos/Visual Studio 2022/E1043beecrowd/E1043beecrowd/Program.cs
@@ -0,0 +1,46 @@
+﻿/* Aula 38 Exercício 1043 Beecrowd - Triângulo
+ * 
+ * Leia 3 valores reais (A, B e C) e verifique se eles formam ou não um triângulo. Em caso positivo, calcule o perímetro do triângulo e apresente a mensagem:
+ * 
+ * Perimetro = XX.X
+ * 
+ * Em caso negativo, calcule a área do trapézio que tem A e B como base e C como altura, mostrando a mensagem
+ * 
+ * Area = XX.X
+ * 
+ * Entrada
+ * A entrada contém três valores reais.
+ * 
+ * Saída
+ * O resultado deve ser apresentado com uma casa decimal.
+ * 
+ * Exemplo de Entrada	                Exemplo de Saída
+ * 6.0 4.0 2.0                          Area = 10.0
+ * 
+ * 6.0 4.0 2.1                          Perimetro = 12.1 */
+
+
+using System;
+using System.Globalization;
+
+namespace Program { 
+    internal class Program { 
+        static void Main(string[] args) {
+            double value1, value2, value3, perimetro, area;
+            string[] values = Console.ReadLine().Split(' ');
+
+            value1 = double.Parse(values[0], CultureInfo.InvariantCulture);
+            value2 = double.Parse(values[1], CultureInfo.InvariantCulture);
+            value3 = double.Parse(values[2], CultureInfo.InvariantCulture);
+
+            perimetro = value1 + value2 + value3;
+            area = (value1 + value2) * value3 / 2.0;
+
+            if ((value1 + value2) > value3 && (value1 + value3) > value2 && (value2 + value3) > value1) {
+                Console.WriteLine("Perimetro = " + perimetro.ToString("F1", CultureInfo.InvariantCulture));
+            } else {
+                Console.WriteLine("Area = " + area.ToString("F1", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
