0633307973ce4f9a7c5ed68f41b3360d92ec165f
PBPaschoal <programming.paschoal@gmail.com>
2024-04-06 00:01:13 -0300

    Jogo Forca em progresso

---
+++ Gemini/Projetos/JogoForca/JogoForca/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace JogoForca
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtLetra = new System.Windows.Forms.TextBox();
+            this.btnTentar = new System.Windows.Forms.Button();
+            this.lblPalavra = new System.Windows.Forms.Label();
+            this.lblTentativas = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtLetra
+            // 
+            this.txtLetra.Location = new System.Drawing.Point(516, 295);
+            this.txtLetra.Name = "txtLetra";
+            this.txtLetra.Size = new System.Drawing.Size(100, 20);
+            this.txtLetra.TabIndex = 0;
+            this.txtLetra.TextChanged += new System.EventHandler(this.txtLetra_TextChanged);
+            // 
+            // btnTentar
+            // 
+            this.btnTentar.Location = new System.Drawing.Point(540, 143);
+            this.btnTentar.Name = "btnTentar";
+            this.btnTentar.Size = new System.Drawing.Size(75, 23);
+            this.btnTentar.TabIndex = 1;
+            this.btnTentar.Text = "button1";
+            this.btnTentar.UseVisualStyleBackColor = true;
+            // 
+            // lblPalavra
+            // 
+            this.lblPalavra.AutoSize = true;
+            this.lblPalavra.Location = new System.Drawing.Point(351, 199);
+            this.lblPalavra.Name = "lblPalavra";
+            this.lblPalavra.Size = new System.Drawing.Size(35, 13);
+            this.lblPalavra.TabIndex = 2;
+            this.lblPalavra.Text = "label1";
+            // 
+            // lblTentativas
+            // 
+            this.lblTentativas.AutoSize = true;
+            this.lblTentativas.Location = new System.Drawing.Point(247, 342);
+            this.lblTentativas.Name = "lblTentativas";
+            this.lblTentativas.Size = new System.Drawing.Size(35, 13);
+            this.lblTentativas.TabIndex = 3;
+            this.lblTentativas.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblTentativas);
+            this.Controls.Add(this.lblPalavra);
+            this.Controls.Add(this.btnTentar);
+            this.Controls.Add(this.txtLetra);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtLetra;
+        private System.Windows.Forms.Button btnTentar;
+        private System.Windows.Forms.Label lblPalavra;
+        private System.Windows.Forms.Label lblTentativas;
+    }
+}
+
---
+++ Gemini/Projetos/JogoForca/JogoForca/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JogoForca
+{
+    public partial class Form1 : Form
+    {
+        // Array de palavras
+        string[] palavras = { "banana", "maçã", "casa", "mesa", "livro" };
+
+        // Variáveis do jogo
+        string palavraSecreta;
+        string palavraComLetras = "";
+        int tentativas = 6;
+        int erros = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Gerar palavra secreta
+            palavraSecreta = palavras[new Random().Next(palavras.Length)];
+
+            // Inicializar palavra com letras escondidas
+            palavraComLetras = new string('_', palavraSecreta.Length);
+
+            // Atualizar labels
+            lblPalavra.Text = palavraComLetras;
+            lblTentativas.Text = "Tentativas Restantes: " + tentativas.ToString();
+        }
+
+        private void btnTentar_Click(object sender, EventArgs e)
+        {
+            // Obter letra digitada
+            string letra = txtLetra.Text.ToLower();
+
+            if (tentativas > 0)
+            {
+                if (palavraSecreta.Contains(letra))
+                {
+                    // Atualizar palavra com as letras acertadas
+                    for (int i = 0; i < palavraSecreta.Length; i++)
+                    {
+                        if (palavraSecreta.Contains(letra))
+                        {
+                            // Atualizar palavra com as letras acertadas
+                            StringBuilder novaPalavra = new StringBuilder(palavraComLetras);
+                            for (i = 0; i < palavraSecreta.Length; i++)
+                            {
+                                if (palavraSecreta[i] == letra[0]) // Verifica se a letra está correta
+                                {
+                                    novaPalavra[i] = letra[0]; // Atualiza a letra na posição correta
+                                }
+                            }
+
+                            palavraComLetras = novaPalavra.ToString(); // Atualiza a palavraComLetras com a nova palavra construída
+
+                            // Atualizar labels
+                            lblPalavra.Text = palavraComLetras;
+                        }
+                    }
+
+                    // Atualizar labels
+                    lblPalavra.Text = palavraComLetras;
+
+                    // Verificar se a palavra foi completada
+                    if (palavraComLetras == palavraSecreta)
+                    {
+                        MessageBox.Show("Parabéns, você adivinhou a palavra!");
+                        btnTentar.Enabled = false;
+                    }
+                }
+                else
+                {
+                    // Aumentar o número de erros
+                    erros++;
+
+                    // Atualizar labels
+                    lblTentativas.Text = lblTentativas.Text.Replace("Tentativas", "Tentativas Restantes: " + (tentativas - erros).ToString());
+
+                    // Verificar se o jogador perdeu
+                    if (erros == tentativas)
+                    {
+                        MessageBox.Show("Você perdeu! A palavra secreta era " + palavraSecreta);
+                        btnTentar.Enabled = false;
+                    }
+                }
+
+                // Limpar a caixa de texto
+                txtLetra.Text = "";
+            }
+        }
+    }
+}
