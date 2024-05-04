d41ecc87cb1eb055762859ad2110c3fb7ac8b642
PBPaschoal <programming.paschoal@gmail.com>
2024-05-04 18:01:26 -0300

    Exercício Beecrowd 1096 - Sequencia IJ 2

---
+++ Udemy 2024/Curso - Primeiros Passos/Seção 05 - Estruturas repetitivas/Aula 52 - Exercícios Beecrowd/E1096beecrowd/E1096beecrowd/Program.cs
@@ -0,0 +1,39 @@
+﻿/* Aula 52 Exercício 1096 Beecrowd Sequencia IJ 2
+ * Você deve fazer um programa que apresente a sequencia conforme o exemplo abaixo.
+ * 
+ * Entrada
+ * Não há nenhuma entrada neste problema.
+ *
+ * Saída
+ * Imprima a sequencia conforme exemplo abaixo
+ * 
+ * Exemplo de Entrada	       Exemplo de Saída
+ *                             I=1 J=7
+ *                             I=1 J=6
+ *                             I=1 J=5
+ *                             I=3 J=7
+ *                             I=3 J=6
+ *                             I=3 J=5
+ *                             ...
+ *                             I=9 J=7
+ *                             I=9 J=6
+  *                            I=9 J=5 */
+
+using System;
+using System.Globalization;
+
+namespace Program { 
+    
+    internal class Program { 
+        
+        static void Main(string[] args) {
+            for (int i = 1; i <= 9; i += 2)
+            { // Incrementa de 2 em 2, conforme o exemplo
+                for (int j = 7; j >= 5; j--)
+                { // Decrementa de 1 em 1, conforme o exemplo
+                    Console.WriteLine("I=" + i + " J=" + j);
+                }
+            }
+        }
+    }
+}
