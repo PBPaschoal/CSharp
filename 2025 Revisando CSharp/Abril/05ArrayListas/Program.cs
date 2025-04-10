c3c029bbbd9534586e45bffad96b40f7dae72aef
Paulo Bruno <programming.paschoal@gmail.com>
2025-04-10 20:27:37 -0300

    Arrays e Listas

---
+++ 2025 Revisando CSharp/Abril/05ArrayListas/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Program {
+
+    internal class  Program {
+
+        public static void Main(string[] args)
+        {
+            //Array (vetor fixo)
+
+            int[] numeros = new int[3];
+
+            numeros[0] = 10;
+            numeros[1] = 20;
+            numeros[2] = 30;
+
+            for (int i = 0; i < numeros.Length; i++) { 
+                Console.WriteLine("Valor: " + numeros[i]);
+            }
+
+            //Array com valores direto
+            string[] nomes = { "Ana", "João", "Paulo" };
+
+            foreach (string nome in nomes) {
+                Console.WriteLine("Nome: " + nome);
+            }
+
+            //Lista (flexível, pode crescer)
+            List<string> frutas = new List<string>();
+            frutas.Add("Maçã");
+            frutas.Add("Banana");
+            frutas.Add("Uva");
+
+            foreach (string fruta in frutas) {
+                Console.WriteLine("Fruta: " + fruta);
+            }
+        }
+    }
+}
