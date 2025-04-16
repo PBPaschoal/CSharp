a605c5da88688e1907242da1e29615501494e754
Paulo Bruno <programming.paschoal@gmail.com>
2025-04-15 21:28:07 -0300

    Split

---
+++ 2025 Revisando CSharp/Revisando curso Lógica e Algoritmos/Aula 017/entradaDeDadosSplit/Program.cs
@@ -0,0 +1,11 @@
+﻿string s = Console.ReadLine();
+
+//string[] v = s.Split(' ');
+string[] v = Console.ReadLine().Split(' ');
+string a = v[0];
+string b = v[1]; 
+string c = v[2];
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
