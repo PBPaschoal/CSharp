1fa83e2437608a89c6028d94a3284265866822cf
Paulo Bruno <programming.paschoal@gmail.com>
2025-04-17 17:46:31 -0300

    Criando o primeiro projeto do Livro Use a Cabeça C#

---
+++ 2025 Revisando CSharp/Livros/Use a Cabeça C#/Contatos/Form1.cs
@@ -0,0 +1,15 @@
+namespace Contatos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Contact List 1.0.\nWritten by: Your Name", "About");
+        }
+    }
+}
