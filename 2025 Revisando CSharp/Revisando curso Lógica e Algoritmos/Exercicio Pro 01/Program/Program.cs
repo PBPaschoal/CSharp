c4a5953aa68bbfa7299f6c60996f89b3db7b4d97
Paulo Bruno <programming.paschoal@gmail.com>
2025-04-21 21:09:02 -0300

    Exercício Proposto 01 - Revisando Curso C# Primeiros Passos: Lógica de Programação e Algoritmos - Aula 18

---
+++ 2025 Revisando CSharp/Revisando curso Lógica e Algoritmos/Exercicio Pro 01/Program/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Program {
+
+    public class Program {
+
+        public static void Main(string[] args) {
+
+            Console.WriteLine("Entre com o seu nome completo:");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Quantos quartos tem na sua casa?");
+            int quantidade_quartos = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o preço de um produto:");
+            double preco_produto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Entre seu último nome, idade e altura (mesma linha):");
+            string[] vet = Console.ReadLine().Split(' ');
+            string sobrenome = vet[0];
+            int idade = int.Parse(vet[1]);
+            double altura = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            Console.WriteLine(nome);
+            Console.WriteLine(quantidade_quartos);
+            Console.WriteLine(preco_produto.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(sobrenome);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
