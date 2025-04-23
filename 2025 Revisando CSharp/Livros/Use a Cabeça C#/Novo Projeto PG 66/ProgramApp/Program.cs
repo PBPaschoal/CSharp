a2d6535cf8a03d30d66e92eac14df0394ab4d0b5
Paulo Bruno <programming.paschoal@gmail.com>
2025-04-22 23:26:57 -0300

    Testes e exemplos - Depurando; Variaveis; Estruturas condicionais e de repetições

--- 2025 Revisando CSharp/Livros/Use a Cabeça C#/Novo Projeto PG 66/ProgramApp/Program.cs
+++ 2025 Revisando CSharp/Livros/Use a Cabeça C#/Novo Projeto PG 66/ProgramApp/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace WindowsFormsApp1
+namespace ProgramApp
 {
     internal static class Program
     {
@@ -12,7 +12,7 @@ namespace WindowsFormsApp1
         /// Ponto de entrada principal para o aplicativo.
         /// </summary>
         [STAThread]
-        static void NotMain()
+        static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
---
+++ 2025 Revisando CSharp/Livros/Use a Cabeça C#/Teste 01/WindowsFormsApp1/AnotherClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1 {
+
+    internal class AnotherClass {
+
+        public static void Main(string[] args) {
+
+            MessageBox.Show("Pow!");
+        }
+    }
+}
