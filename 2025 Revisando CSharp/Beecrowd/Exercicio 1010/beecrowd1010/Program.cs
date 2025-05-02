f1ca50d5de635de71502438b7b9e6c4bf1ed8f43
Paulo Bruno <programming.paschoal@gmail.com>
2025-05-02 15:40:13 -0300

    Exercício beecrowd 1010 - Cálculo Simples | Ajustando README

---
+++ 2025 Revisando CSharp/Beecrowd/Exercicio 1010/beecrowd1010/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace Program {
+
+    public class Program {
+
+        public static void Main(string[] args)
+        {
+            string[] product01 = Console.ReadLine().Split(' ');
+            // Primeira peça
+            int partCode01 = int.Parse(product01[0]); // Código da peça 01
+            int quantityOfPieces01 = int.Parse(product01[1]); // Quantidade da peça 01
+            double unitPricePerPiece01 = double.Parse(product01[2], CultureInfo.InvariantCulture); // Valor unitário 01
+
+            string[] product02 = Console.ReadLine().Split(' ');
+            // Segunda Peça
+            int partCode02 = int.Parse(product02[0]); // Código da peça 02
+            int quantityOfPieces02 = int.Parse(product02[1]); // Quantidade da peça 02
+            double unitPricePerPiece02 = double.Parse(product02[2], CultureInfo.InvariantCulture); // Valor unitário 02
+
+            double amountToPay = (quantityOfPieces01 * unitPricePerPiece01) + (quantityOfPieces02 * unitPricePerPiece02); // Valor total a pagar
+
+            Console.WriteLine("VALOR A PAGAR: R$ " + amountToPay.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
