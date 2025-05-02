65239d6e28fa9764ba40ef46b1e1b330022eac18
Paulo Bruno <programming.paschoal@gmail.com>
2025-05-02 13:59:47 -0300

    Exercćio 1008 Diferença - beecrowd

---
+++ 2025 Revisando CSharp/Beecrowd/Exercicio 1008/beecrowd1008/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Program {
+
+    public class Program {
+
+        public static void Main(string[] args) {
+
+            int idEmployee = int.Parse(Console.ReadLine()); // Número de identificação do funcionario
+            int hoursWorkedEmployee = int.Parse(Console.ReadLine()); // Número de horas trabalhadas pelo funcionario
+            double hourlyRateEmployee = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture); // Salário por hora
+
+            double wageEmployee = (double)hoursWorkedEmployee * hourlyRateEmployee; 
+            // Calculando valor por hora pela hora trabalhada para saber o salario total do funcionario
+
+            Console.WriteLine("NUMBER = " + idEmployee);
+            Console.WriteLine("SALARY = U$ " + wageEmployee.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
