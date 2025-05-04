32b423d5c976cdf3114a8bad593e6c110f0cff50
Paulo Bruno <programming.paschoal@gmail.com>
2025-05-03 22:45:17 -0300

    Exercicio 1011 - Esfera | beecrowd

---
+++ 2025 Revisando CSharp/Beecrowd/Exercicio 1011/beecrowd1011/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace Program {
+
+    public class Program {
+
+        public static void Main(string[] args) {
+
+            double pi = 3.14159;
+            double ray = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double volume = (4.0 / 3.0) * pi * Math.Pow(ray, 3.0);
+
+            Console.WriteLine("VOLUME = " + volume.ToString("F3", CultureInfo.InvariantCulture));
+        }
+    }
+}
