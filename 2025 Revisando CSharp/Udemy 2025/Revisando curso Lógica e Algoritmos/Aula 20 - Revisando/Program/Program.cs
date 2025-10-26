a2b127ee7041100c464930dc4121cf685e3313c3
Paulo Bruno <programming.paschoal@gmail.com>
2025-10-26 20:48:41 -0300

    Revisando aula 20

---
+++ 2025 Revisando CSharp/Udemy 2025/Revisando curso Lógica e Algoritmos/Aula 20 - Revisando/Program/Program.cs
@@ -0,0 +1,32 @@
+﻿//Aula 20 Exercício 01
+//Fazer um programa para ler as medidas da largura e comprimento de um terreno retangular com uma
+//casa decimal, bem como o valor do metro quadrado do terreno com duas casas decimais.
+//Em seguida, o programa deve mostrar o valor da área do terreno, bem como o valor do preço do
+//terreno, ambos com duas casas decimais, conforme exemplo.
+//Entrada:
+//10.0
+//30.0
+//200.00
+//Saída:
+//AREA = 300.00
+//PRECO = 60000.00
+
+using System;
+using System.Globalization;
+
+namespace Program
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            double largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double comprimento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double area = largura * comprimento;
+            double precototal = area * preco;
+            Console.WriteLine("AREA = " + area.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PRECO = " + precototal.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
