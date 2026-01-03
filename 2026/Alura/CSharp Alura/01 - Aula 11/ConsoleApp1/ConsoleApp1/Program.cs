71675e3ed8a4878aa96c282023cd46f486f67162
Bruno Paschoal <programming.paschoal@gmail.com>
2026-01-03 07:09:06 -0300

    Adiciona .gitignore e remove bin/obj do versionamento

---
+++ 2026/Alura/CSharp Alura/01 - Aula 11/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Criar uma variável chamada curso, e guarde nela o nome do curso estudado.
+// Mostrar o conteúdo da variável curso no console.
+// Criar uma variável chamada nome e outra chamada sobrenome e guardar nelas seu nome e sobrenome.
+// Mostrar na tela o nome dos instrutores do curso.
+// 01 Aula 11 
+
+string curso = "C# criando sua primeira aplicação";
+Console.WriteLine(curso);
+string nome = "Paulo";
+string sobrenome = "Paschoal";
+Console.WriteLine("Daniel Portugal e Guilherme Lima");
