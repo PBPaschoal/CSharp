95ab2010bcc2f7804ae5a1a42f546da875e507a9
Bruno Paschoal <programming.paschoal@gmail.com>
2026-01-15 21:31:29 -0300

    Projeto ScreenSound - criando álbum e exibindo músicas do álbum

---
+++ 2026/Alura/02 - Aplicando a Orientação a Objetos/ScreenSound/ScreenSound/Album.cs
@@ -0,0 +1,19 @@
+﻿class Album {
+
+    private List<Musica> musicas = new List<Musica>();
+    public string Nome { get; set; }
+
+    public int DuracaoTotal => musicas.Sum(m => m.Duracao);
+
+    public void AdicionarMusica(Musica musica) {
+        musicas.Add(musica);
+    }
+
+    public void ExibirMusicasAlbum() {
+        Console.WriteLine($"Lista de músicas do álbum {Nome}:\n");
+        foreach (var musica in musicas) {
+            Console.WriteLine($"Música: {musica.Nome}");
+        }
+        Console.WriteLine($"\nPara ouvir este álbum inteiro você precisa de {DuracaoTotal}");
+    }
+}
--- 2026/Alura/02 - Aplicando a Orientação a Objetos/ScreenSound/ScreenSound/Program.cs
+++ 2026/Alura/02 - Aplicando a Orientação a Objetos/ScreenSound/ScreenSound/Program.cs
@@ -1,16 +1,15 @@
-﻿Musica musica1 = new Musica();
-musica1.Nome = "Roxane";
-musica1.Artista = "The Police";
-musica1.Duracao = 273;
-musica1.Disponivel = true;
-Console.WriteLine(musica1.DescricaoResumida);
+﻿Album albumDoQueen = new Album();
+albumDoQueen.Nome = "A night at the opera";
+
+Musica musica1 = new Musica();
+musica1.Nome = "Love of my life";
+musica1.Duracao = 213;
 
 Musica musica2 = new Musica();
-musica2.Nome = "Vertigo";
-musica2.Artista = "U2";
-musica2.Duracao = 367;
-musica2.Disponivel = true;
-Console.WriteLine(musica2.Disponivel);
+musica2.Nome = "Bohemian Rapsody";
+musica2.Duracao = 354;
 
-musica1.ExibirFichaTecnica();
-musica2.ExibirFichaTecnica();
+albumDoQueen.AdicionarMusica(musica1);
+albumDoQueen.AdicionarMusica(musica2);
+
+albumDoQueen.ExibirMusicasAlbum();
