8a6bd8130d7ec22320a3aca330cab614fe9a453a
Bruno Paschoal <programming.paschoal@gmail.com>
2026-01-18 23:16:30 -0300

    Exercício do Livro

---
+++ 2026/Livro Use a Cabeça CSharp/PG 70/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿int result = 0;
+int x = 6;
+while (x > 3) {
+    result = result + x;
+    x = x - 1;
+}
+
+for (int z = 1; z < 3; z = z + 1) {
+    result = result + z;
+}
+
+Console.WriteLine("The result is " + result);
+Console.ReadLine();
