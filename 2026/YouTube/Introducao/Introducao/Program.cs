0bbc4506c8a6d3a4078183a209000e6ffe6a9793
Bruno Paschoal <programming.paschoal@gmail.com>
2026-01-20 21:08:54 -0300

    Vídeos para o canal no YouTube - exemplos de código

---
+++ 2026/YouTube/Introducao/Introducao/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Olá, Mundo!");
+Console.WriteLine("Bem-vindos ao canal Bruno Paschoal | Linha de Código");
+Console.WriteLine();
+Console.Write("Hoje nós iremos aprender: ");
+Console.WriteLine("Saída de dados!");
+Console.Write("\nVamos nessa!");
---
+++ 2026/YouTube/Program/Program/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine();
