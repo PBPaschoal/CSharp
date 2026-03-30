d9d2cee8dce61317d678f11b3b2bbe4464db19ef
Paulo Bruno Paschoal <programming.paschoal@gmail.com>
2026-03-29 22:17:28 -0300

    Parte 02 Aula 08: Quantos ímpares foram digitados?

---
+++ Estudos 2026/Alura 2026/05 - Praticando CSharp - laços de repetição/Parte 02 - Aula 08/Program.cs
@@ -0,0 +1,36 @@
+﻿/* Você está desenvolvendo uma ferramenta de análise de dados que permite ao usuário inserir 10 números inteiros, um por vez. 
+O objetivo é contar quantos desses números são ímpares e, ao final, exibir o total de números ímpares digitados.
+
+Para isso, você precisará de um programa que:
+
+    Solicite ao usuário a inserção de 10 números inteiros.
+    Verifique, para cada número, se ele é ímpar.
+    Conte quantos números ímpares foram digitados.
+    Exiba o total de números ímpares ao final.
+
+Exemplo de entrada:
+
+Digite um número: 2
+Digite um número: 3
+Digite um número: 5
+Digite um número: 7
+Digite um número: 10
+Digite um número: 12
+Digite um número: 15
+Digite um número: 17
+Digite um número: 20
+Digite um número: 21
+
+Saída esperada:
+
+Você digitou 6 números ímpares. */
+
+int cont = 0;
+for(int i = 0; i < 10; i++) {
+    Console.Write("Digite um número: ");
+    int digite = int.Parse(Console.ReadLine());
+    if(digite % 2 != 0) {
+        cont++;
+    }
+}
+Console.WriteLine($"Você digitou {cont} números ímpares.");
