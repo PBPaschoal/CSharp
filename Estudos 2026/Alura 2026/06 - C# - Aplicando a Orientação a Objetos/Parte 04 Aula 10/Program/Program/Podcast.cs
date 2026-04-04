513b5722135f7333a85b9d509d29f5f722769bdb
Bruno Paschoal <programming.paschoal@gmail.com>
2026-04-04 01:33:43 -0300

    Projeto final concluido

---
+++ Estudos 2026/Alura 2026/06 - C# - Aplicando a Orientação a Objetos/Parte 04 Aula 10/Program/Program/Podcast.cs
@@ -0,0 +1,26 @@
+﻿class Podcast {
+
+    private List<Episodio> episodios = new();
+    public Podcast(string nome, string host)
+    {
+        Nome = nome;
+        Host = host;
+    }
+
+    public string Nome { get; set;  }
+    public string Host { get; set; }
+
+    public int TotalEpisodios => episodios.Count;
+
+    public void AdicionarEpisodio(Episodio episodio) { 
+        episodios.Add(episodio);
+    }
+
+    public void ExibirDetalhes() {
+        Console.WriteLine($"Podcast {Nome} apresentado por {Host}\n");
+        foreach (Episodio episodio in episodios.OrderBy(e => e.Ordem)) {
+            Console.WriteLine(episodio.Resumo);
+        }
+        Console.WriteLine($"\n\nEste podcast possui {TotalEpisodios}. episodios");
+    }
+}
---
+++ Estudos 2026/Alura 2026/06 - C# - Aplicando a Orientação a Objetos/Parte 04 Aula 10/Program/Program/Program.cs
@@ -0,0 +1,20 @@
+﻿Episodio ep1 = new(1, "Técnicas de facilitação", 45);
+ep1.AdicionarConvidados("Maria");
+ep1.AdicionarConvidados("Marcelo");
+Console.WriteLine(ep1.Resumo);
+
+Episodio ep2 = new(2, "Minhas gatas", 67);
+ep2.AdicionarConvidados("Pandora");
+ep2.AdicionarConvidados("Lolla");
+Console.WriteLine(ep2.Resumo);
+
+Episodio ep3 = new(3, "Meus gatos", 67);
+ep3.AdicionarConvidados("Pepe Antonio");
+ep3.AdicionarConvidados("Simon");
+Console.WriteLine(ep3.Resumo);
+
+Podcast podcast = new("Podecast especial", "Bruno");
+podcast.AdicionarEpisodio(ep1);
+podcast.AdicionarEpisodio(ep2);
+podcast.AdicionarEpisodio(ep3);
+podcast.ExibirDetalhes();
