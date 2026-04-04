614fb88f8219547e94a4a33dd4ef33c7b5d7cb23
Paulo Bruno Paschoal <programming.paschoal@gmail.com>
2026-04-04 01:03:34 -0300

    Iniciando o desafio Parte 05

---
+++ Estudos 2026/Alura 2026/06 - C# - Aplicando a Orientação a Objetos/Parte 04 Aula 10/Program.cs
@@ -0,0 +1,38 @@
+﻿/* Para ajudar nesse desafio, vou deixar algumas sugestões de classes que você pode implementar criando suas próprias regras de negócio:
+
+    Classe ReprodutorMusical: Responsável por controlar a reprodução de músicas, incluindo reprodução, pausa, avanço, retrocesso, controle de volume, etc.
+
+    Classe Playlist: Representa uma coleção de músicas organizadas em uma determinada ordem ou tema.
+
+    Classe Artista: Representa um artista musical, contendo informações como nome, biografia, lista de álbuns, etc.
+
+    Classe GeneroMusical: Representa um gênero musical, contendo informações como nome, descrição, lista de artistas ou músicas associadas, etc.
+
+    Classe Usuario: Representa um usuário da aplicação, com informações como nome, e-mail, senha, histórico de reprodução, playlists criadas, etc.
+
+    Classe AvaliacaoMusica: Representa uma avaliação feita por um usuário em relação a uma música, contendo informações como a pontuação e o comentário.
+
+    Classe Busca: Responsável por realizar pesquisas de músicas, artistas, álbuns ou playlists com base em critérios fornecidos pelo usuário.
+
+    Classe Recomendacao: Responsável por gerar recomendações personalizadas de músicas com base nos hábitos de audição do usuário.
+
+    Classe LetraMusica: Representa a letra de uma música, permitindo a exibição ou busca de letras.
+
+    Classe EstatisticasUsuario: Responsável por coletar e calcular estatísticas relacionadas ao uso da aplicação por parte dos usuários, como músicas mais reproduzidas, artistas favoritos, etc.
+
+    Classe Notificacao: Responsável por gerenciar notificações enviadas aos usuários, como atualizações de playlists, lançamentos de álbuns, etc.
+
+    Classe Compartilhamento: Permite que os usuários compartilhem músicas, playlists ou artistas com outros usuários.
+
+    Classe LojaVirtual: Representa uma loja virtual de músicas, permitindo a compra ou download de músicas ou álbuns.
+
+    Classe Podcast: Representa um episódio de um podcast, contendo informações como título, descrição, duração, etc.
+
+    Classe DownloadManager: Responsável por gerenciar o download de músicas ou álbuns para uso offline.
+
+    Classe EventoMusical: Representa um evento musical, como um show ao vivo ou um festival, contendo informações como local, data, lista de artistas participantes, etc.
+
+    Classe RadioOnline: Permite a reprodução contínua de músicas em uma sequência aleatória ou baseada em um gênero ou artista específico.
+
+    Classe ContaPremium: Representa uma conta premium ou assinatura paga, desbloqueando recursos adicionais, como reprodução ilimitada, qualidade de áudio superior, etc. */
+
