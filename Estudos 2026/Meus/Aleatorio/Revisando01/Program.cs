675bc57fb2864243904d07d6d6bd8d4f62fe3b6d
Paulo Bruno <programming.paschoal@gmail.com>
2026-04-17 23:11:56 -0300

    Revisando sem consulta - ainda não terminei

---
+++ Estudos 2026/Meus/Aleatorio/Revisando01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Globalization;
+Console.WriteLine("---------------------------------------");
+Console.WriteLine("Revisando algumas coisas sem consultas!");
+Console.WriteLine("---------------------------------------");
+
+Console.WriteLine("Vamos contar até que número? ");
+int ateQuanto = int.Parse(Console.ReadLine());
+Console.WriteLine($"\nContando até {ateQuanto}:");
+for(int i = 0; i <= ateQuanto; i++){
+    Console.WriteLine(i);
+}
+
+Console.WriteLine("\n------------------------");
+Console.Write("Informe o seu nome: ");
+string nome = Console.ReadLine();
+Console.Write("Informe a sua idade: ");
+byte idade = Byte.Parse(Console.ReadLine());
+Console.Write("Informe o seu sexo: ");
+char sexo = Char.Parse(Console.ReadLine());
+Console.Write("Informe a sua altura: ");
+double altura = Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+string frase;
+if(sexo == 'F' || sexo == 'f') {
+    frase = "uma mulher";
+} else {
+    frase = "um homem";
+}
+string frase2;
+if(altura >= 1.90) {
+    frase2 = "muito alta";
+}   else if(altura >= 1.70) {
+        frase2 = "alta";
+    } else {
+        frase2 = "baixa";
+    }
+
+Console.WriteLine($"Olá {nome}!\nObrigado pelas informações.\nVerifiquei que você é {frase} e tem {idade} anos. E pela a sua altura, você é uma pessoa {frase2}.");
