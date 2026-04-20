291569923bfca06bbd930530be2aa05affdb416c
Bruno Paschoal <programming.paschoal@gmail.com>
2026-04-20 19:29:24 -0300

    Parte 01 Aula 04 - Atributos e métodos

---
+++ Estudos 2026/Alura 2026/07 - Orientação a objetos com classes/Parte 02 Aula 04/Program/Program/Produto.cs
@@ -0,0 +1,14 @@
+﻿class Produto {
+    public string nome;
+    public string descricao;
+    public decimal preco;
+    public int estoque;
+
+    public bool EstaDisponivel() {
+        return estoque > 0;
+    }
+
+    public void AlterarPrecoComDesconto(decimal desconto) {
+        preco = preco * (1 - desconto);
+    }
+}
---
+++ Estudos 2026/Alura 2026/07 - Orientação a objetos com classes/Parte 02 Aula 04/Program/Program/Program.cs
@@ -0,0 +1,51 @@
+﻿
+string nome2 = "Cadeira gamer";
+string descricao2 = "Conforto ergonômico com design moderno para longas sessões de jogo.";
+decimal preco2 = 600.00m;
+int estoque2 = 5;
+
+Console.WriteLine();
+Console.WriteLine(@$"Dados do item 2:
+Nome: {nome2};
+Descrição: {descricao2};
+Preço: {preco2};
+Estoque: {estoque2}");
+
+Produto item1 = new Produto();
+item1.nome = "Teclado";
+item1.descricao = "Modelo compacto e silencioso, perfeito para produtividade diária.";
+item1.preco = 80.00m;
+item1.estoque = 15;
+
+Console.WriteLine();
+Console.WriteLine(@$"Dados do item 1:
+Nome: {item1.nome};
+Descrição: {item1.descricao};
+Preço: {item1.preco};
+Estoque: {item1.estoque}");
+
+if (item1.EstaDisponivel()) {
+    Console.WriteLine("O produto está disponível!");
+} 
+
+item1.AlterarPrecoComDesconto(0.2m);
+
+Console.WriteLine();
+Console.WriteLine(@$"Dados do item 1:
+Nome: {item1.nome};
+Descrição: {item1.descricao};
+Preço: {item1.preco};
+Estoque: {item1.estoque}");
+
+Produto item3 = new Produto();
+item3.nome = "Mouse";
+item3.descricao = "Modelo compacto e silencioso, perfeito para produtividade diária.";
+item3.preco = 40.00m;
+item3.estoque = 0;
+
+Console.WriteLine();
+Console.WriteLine(@$"Dados do item 3:
+Nome: {item3.nome};
+Descrição: {item3.descricao};
+Preço: {item3.preco};
+Estoque: {item3.estoque}");
