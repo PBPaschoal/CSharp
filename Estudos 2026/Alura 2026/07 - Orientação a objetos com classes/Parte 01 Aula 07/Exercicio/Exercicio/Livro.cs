b7bec8d514a386db057f997eda4643edb3033c32
Bruno Paschoal <programming.paschoal@gmail.com>
2026-04-20 19:47:09 -0300

    Parte 01 Aula 07: Exercício - Faça como eu fiz: cadastro de livros na biblioteca

---
+++ Estudos 2026/Alura 2026/07 - Orientação a objetos com classes/Parte 01 Aula 07/Exercicio/Exercicio/Livro.cs
@@ -0,0 +1,5 @@
+﻿class Livro {
+
+    public string Titulo { get; set; }
+    public string Autor {  get; set; }
+}
---
+++ Estudos 2026/Alura 2026/07 - Orientação a objetos com classes/Parte 01 Aula 07/Exercicio/Exercicio/Program.cs
@@ -0,0 +1,6 @@
+﻿Livro meuLivro =  new Livro();
+
+meuLivro.Titulo = "Harry Potter e a Pedra Filosofal";
+meuLivro.Autor = "J.K. Rowling";
+Console.WriteLine("Livro: " + meuLivro.Titulo);
+Console.WriteLine("Escritora : " + meuLivro.Autor);
