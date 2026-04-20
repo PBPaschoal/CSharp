5d83c52a49c8aa81a1086658dd09989d02ec71b5
Bruno Paschoal <programming.paschoal@gmail.com>
2026-04-20 08:15:33 -0300

    Exercício 1004 Produto Simples Beecrowd

---
+++ Estudos 2026/Beecrowd/Ex1004/Ex1004/Program.cs
@@ -0,0 +1,33 @@
+﻿/* Leia dois valores inteiros. A seguir, calcule o produto entre estes dois valores e atribua esta operação à variável PROD. A seguir mostre a variável PROD com mensagem correspondente.   
+
+Entrada
+O arquivo de entrada contém 2 valores inteiros.
+
+Saída
+Imprima a mensagem "PROD" e a variável PROD conforme exemplo abaixo, com um espaço em branco antes e depois da igualdade. Não esqueça de imprimir o fim de linha após o produto, caso contrário seu programa apresentará a mensagem: “Presentation Error”.
+
+Exemplos de Entrada	Exemplos de Saída
+3
+9
+
+PROD = 27
+
+-30
+10
+
+PROD = -300
+
+0
+9
+
+PROD = 0 */
+
+using System;
+class Program {
+    static void Main(string[] args) {
+        int valor1 = int.Parse(Console.ReadLine());
+        int valor2 = int.Parse(Console.ReadLine());
+        int PROD = valor1 * valor2;
+        Console.WriteLine($"PROD = {PROD}");
+    }
+}
