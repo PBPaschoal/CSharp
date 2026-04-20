7fb1f8ccd10ebf913209b4c0242c12344d4020b0
Bruno Paschoal <programming.paschoal@gmail.com>
2026-04-20 09:42:33 -0300

    Iniciando curso: Praticando C#: orientação a objtos com classes. Parte 01 Aula 03 Classes e objetos

---
+++ Estudos 2026/Alura 2026/07 - Orientação a objetos com classes/Parte 01 Aula 03/Program/Program/Produto.cs
@@ -0,0 +1,6 @@
+﻿class Produto {
+    public string nome;
+    public string descricao;
+    public decimal preco;
+    public int estoque;
+}
---
+++ Estudos 2026/Alura 2026/07 - Orientação a objetos com classes/Parte 01 Aula 03/Program/Program/Program.cs
@@ -0,0 +1,23 @@
+﻿
+string nome2 = "Cadeira gamer";
+string descricao2 = "Conforto ergonômico com design moderno para longas sessões de jogo.";
+decimal preco2 = 600.00m;
+int estoque2 = 5;
+
+Console.WriteLine(@$"Dados do item 2:
+Nome: {nome2};
+Descrição: {descricao2};
+Preço: {preco2};
+Estoque: {estoque2}");
+
+Produto item1 = new Produto();
+item1.nome = "Teclado";
+item1.descricao = "Modelo compacto e silencioso, perfeito para produtividade diária.";
+item1.preco = 80.00m;
+item1.estoque = 15;
+
+Console.WriteLine(@$"Dados do item 1:
+Nome: {item1.nome};
+Descrição: {item1.descricao};
+Preço: {item1.preco};
+Estoque: {item1.estoque}");
