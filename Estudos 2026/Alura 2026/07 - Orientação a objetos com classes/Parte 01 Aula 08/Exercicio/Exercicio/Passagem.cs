2201e60808b1a2306581df91cc423d4611d4cd0e
Bruno Paschoal <programming.paschoal@gmail.com>
2026-04-20 21:25:06 -0300

    Parte 01 Aula 08 Exercício: Faça como eu fiz: emissão de passagem de viagem

---
+++ Estudos 2026/Alura 2026/07 - Orientação a objetos com classes/Parte 01 Aula 08/Exercicio/Exercicio/Passagem.cs
@@ -0,0 +1,11 @@
+﻿class Passagem {
+
+    public string Passageiro { get; set; }
+    public string Destino { get; set; }
+
+    public Passagem(string passageiro, string destino) { 
+        Passageiro = passageiro;
+        Destino = destino;
+    }
+
+}
---
+++ Estudos 2026/Alura 2026/07 - Orientação a objetos com classes/Parte 01 Aula 08/Exercicio/Exercicio/Program.cs
@@ -0,0 +1,3 @@
+﻿Passagem passagem = new Passagem("Bruno", "Japão");
+Console.WriteLine($"Passageiro: {passagem.Passageiro}");
+Console.WriteLine($"Destino: {passagem.Destino}");
