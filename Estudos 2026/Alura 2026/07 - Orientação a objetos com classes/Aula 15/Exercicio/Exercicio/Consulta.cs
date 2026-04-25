cd49d03d924c10f8a19ae025bd04db6d43c5cb59
Bruno Paschoal <programming.paschoal@gmail.com>
2026-04-25 19:10:43 -0300

    Aula 15 Exercício Faça como eu fiz: agendamento médico

---
+++ Estudos 2026/Alura 2026/07 - Orientação a objetos com classes/Aula 15/Exercicio/Exercicio/Consulta.cs
@@ -0,0 +1,32 @@
+﻿class Consulta { 
+
+    public string NomePaciente { get; set; }
+    public string NomeMedico { get; set; }
+    public DateTime DataConsulta { get; set; }
+
+    private bool foiReagendada;
+
+    public Consulta(string nomepaciente, string nomemedico, DateTime dataconsulta) {
+        this.NomePaciente = nomepaciente;
+        this.NomeMedico = nomemedico;
+        this.DataConsulta = dataconsulta;
+        foiReagendada = false;
+    }
+
+    public void Reagendar(DateTime novaData) { 
+        DataConsulta = novaData;
+        foiReagendada = true;
+    }
+
+    public void ExibirResumo() {
+        Console.WriteLine($"Consulta marcada com {NomeMedico} para o(a) paciente {NomePaciente}.");
+        if (foiReagendada) {
+            Console.WriteLine("Nova data: " + DataConsulta.ToString("dd/MM/yyyy"));
+        }
+        else {
+            Console.WriteLine("Data: " + DataConsulta.ToString("dd/MM/yyyy"));
+        }
+        Console.WriteLine();
+    }
+
+}
---
+++ Estudos 2026/Alura 2026/07 - Orientação a objetos com classes/Aula 15/Exercicio/Exercicio/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Você está criando um sistema para agendamento de consultas em uma clínica. Toda consulta envolve o nome do paciente, o nome do médico e a data marcada. É comum que consultas sejam reagendadas — e seu sistema precisa permitir isso.
+
+Você deve criar uma classe que represente uma consulta com os dados essenciais e um método para reagendar, mantendo o controle da data atualizada.
+
+Crie uma classe chamada Consulta com:
+
+Propriedade pública NomePaciente.
+Propriedade pública NomeMedico.
+Propriedade pública DataConsulta (do tipo DateTime).
+Método Reagendar(DateTime novaData) para atualizar a data.
+Método ExibirResumo() que mostra as informações da consulta, usando "Data:" na primeira exibição e "Nova data:" se ela tiver sido reagendada.
+Exemplo de entrada:
+
+Consulta consulta = new Consulta("João Lima", "Dra. Renata", new DateTime(2025, 5, 20));
+consulta.ExibirResumo();
+consulta.Reagendar(new DateTime(2025, 5, 25));
+consulta.ExibirResumo();
+
+Exemplo de saída:
+
+Consulta marcada com Dra. Renata para o paciente João Lima.
+Data: 20/05/2025
+
+Consulta marcada com Dra. Renata para o paciente João Lima.
+Nova data: 25/05/2025 */
+
+Consulta consulta = new Consulta("João Lima", "Dra. Retana", new DateTime(2026, 4, 29));
+consulta.ExibirResumo();
+consulta.Reagendar(new DateTime(2026, 5, 02));
+consulta.ExibirResumo();
