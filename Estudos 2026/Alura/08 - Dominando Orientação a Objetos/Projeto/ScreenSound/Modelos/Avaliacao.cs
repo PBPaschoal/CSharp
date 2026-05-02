a262887005d6f04bf75d7f957f900421d454c86c
Bruno Paschoal <programming.paschoal@gmail.com>
2026-05-02 20:51:10 -0300

    Usando o novo tipo

---
+++ Estudos 2026/Alura/08 - Dominando Orientação a Objetos/Projeto/ScreenSound/Modelos/Avaliacao.cs
@@ -0,0 +1,10 @@
+﻿using System.Xml;
+
+namespace ScreenSound.Modelos;
+internal class Avaliacao {
+    public Avaliacao(int nota) {
+        Nota = nota;
+    }
+
+    public int Nota { get; }
+}
--- Estudos 2026/Alura/08 - Dominando Orientação a Objetos/Projeto/ScreenSound/Program.cs
+++ Estudos 2026/Alura/08 - Dominando Orientação a Objetos/Projeto/ScreenSound/Program.cs
@@ -1,11 +1,10 @@
 ﻿using ScreenSound.Modelos;
 
 Banda ira = new Banda("Ira!");
-ira.AdicionarNota(10);
-ira.AdicionarNota(8);
-ira.AdicionarNota(6);
+ira.AdicionarNota(new Avaliacao(10));
+ira.AdicionarNota(new Avaliacao(8));
+ira.AdicionarNota(new Avaliacao(6));
 Banda beatles = new("The Beatles");
-
 
 Dictionary<string, Banda> bandasRegistradas = new();
 bandasRegistradas.Add(ira.Nome, ira);
@@ -139,7 +138,7 @@ void AvaliarUmaBanda()
         Banda banda = bandasRegistradas[nomeDaBanda];
         Console.Write($"Qual a nota que a banda {nomeDaBanda} merece: ");
         int nota = int.Parse(Console.ReadLine()!);
-        banda.AdicionarNota(nota);
+        banda.AdicionarNota(new Avaliacao(nota));
         Console.WriteLine($"\nA nota {nota} foi registrada com sucesso para a banda {nomeDaBanda}");
         Thread.Sleep(2000);
         Console.Clear();
