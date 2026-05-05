473976f06ec21f1196fa5b9f6ba310dcbf0f9c73
Bruno Paschoal <programming.paschoal@gmail.com>
2026-05-05 20:35:01 -0300

    Parte 01 Aula 06 - Faça como eu fiz:o poder do encapsulamento

--- Estudos 2026/Alura/08 - Dominando Orientação a Objetos/Projeto/ScreenSound/Modelos/Avaliacao.cs
+++ Estudos 2026/Alura/08 - Dominando Orientação a Objetos/Projeto/ScreenSound/Modelos/Avaliacao.cs
@@ -3,6 +3,8 @@
 namespace ScreenSound.Modelos;
 internal class Avaliacao {
     public Avaliacao(int nota) {
+        if (nota <= 0) nota = 0;
+        if (nota >= 10) nota = 10;
         Nota = nota;
     }
 
