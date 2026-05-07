51e16aa68b87cfe457b32aa70fe91b7a727e4e04
Bruno Paschoal <programming.paschoal@gmail.com>
2026-05-06 22:12:25 -0300

    Melhorando o projeto: Treinando/Entendendo via console, para criar futuramente uma API Web (sem ajuda de IA!) - Código adaptado: Construido um CRUD (Create, Read) funcional. - Encapsulamento; Construtores; Coleções; Estruturas de repetição - Proximo passo: método Exibir deve ser adaptador para retornar uma string (ToString()).

--- Estudos 2026/Meus/Minha Loja Afiliados/MinhaLojaAfiliados/MinhaLojaAfiliados/Produto.cs
+++ Estudos 2026/Meus/Minha Loja Afiliados/MinhaLojaAfiliados/MinhaLojaAfiliados/Produto.cs
@@ -8,6 +8,7 @@ internal class Produto {
     public int Id { get; set; }
     public string Nome { get; set; }
     public string Marca { get; set; }
+    public string Modelo { get; set; }
     public string Descricao { get; set; }
     public string Plataforma { get; set; }
 
@@ -15,39 +16,27 @@ internal class Produto {
     public string Link { get; set; }
     public string ImagemUrl { get; set; }
 
-    /*public Produto(int id, string nome, string marca, string descricao, string plataforma, string categoria, string link, string imagemurl) {
+    public Produto(int id, string nome, string marca, string modelo, string descricao, string plataforma, string categoria, string link, string imagemurl) {
         this.Id = id;
         this.Nome = nome;
         this.Marca = marca;
+        this.Modelo = modelo;
         this.Descricao = descricao;
         this.Plataforma = plataforma;
         this.Categoria = categoria;
         this.Link = link;
         this.ImagemUrl = imagemurl;
-    }*/
-
-    public void Cadastrar() {
-        string[] vetProduto = Console.ReadLine().Split(' ');
-        Id = int.Parse(vetProduto[0]);
-        Nome = vetProduto[1];
-        Marca = vetProduto[2];
-        Descricao = vetProduto[3];
-        Plataforma = vetProduto[4];
-        Categoria = vetProduto[5];
-        Link = vetProduto[6];
-        ImagemUrl = vetProduto[7];
-        // Mas assim não é uma boa ideia, pois o produto pode ter nome composto, como a descrição precisa ser grande, então separados por Split, vai acabar dando erro com informações a mais.
     }
 
     public void Exibir() {
-        Console.WriteLine($"ID: {Id}");
+        Console.WriteLine($"ID do produto: {Id}");
         Console.WriteLine($"Nome: {Nome}");
         Console.WriteLine($"Marca: {Marca}");
-        Console.WriteLine($"Descrição: {Descricao}");
-        Console.WriteLine($"Plataforma Afiliado: {Plataforma}");
-        Console.WriteLine($"Categoria do Produto: {Categoria}");
+        Console.WriteLine($"Modelo: {Modelo}");
+        Console.WriteLine($"Descricao: {Descricao}");
+        Console.WriteLine($"Plataforma: {Plataforma}");
+        Console.WriteLine($"Categoria: {Categoria}");
         Console.WriteLine($"Link para compra: {Link}");
-        Console.WriteLine($"Imagem: {ImagemUrl}");
+        Console.WriteLine($"Link da imagem do produto: {ImagemUrl}");
     }
-
 }
--- Estudos 2026/Meus/Minha Loja Afiliados/MinhaLojaAfiliados/MinhaLojaAfiliados/Program.cs
+++ Estudos 2026/Meus/Minha Loja Afiliados/MinhaLojaAfiliados/MinhaLojaAfiliados/Program.cs
@@ -1,14 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Channels;
 
 namespace MinhaLojaAfiliados;
 internal class Program {
     public static void Main(string[] args) {
-        Produto produtos = new Produto();
-        Console.WriteLine("Informe os dados abaixo:\nID DO PRODUTO; NOME DO PRODUTO; MARCA DO PRODUTO; DESCRIÇÃO DO PRODUTO; PLATAFORMA; CATEGORIA; LINK; URL DA IMAGEM");
+        List<Produto> listaDeProdutos = new List<Produto>();
+        string continuar = "s"; // String para podermos usar ToLower
 
-        produtos.Cadastrar();
-        produtos.Exibir();
+        while (continuar.ToLower() == "s") {
+            Console.WriteLine("\n/// CADASTRAR NOVO PRODUTO ///");
+            Console.Write("ID do produto: ");
+            int id = int.Parse(Console.ReadLine());
+            Console.Write("Nome do produto: ");
+            string nome = Console.ReadLine();
+            Console.Write("Marca: ");
+            string marca = Console.ReadLine();
+            Console.Write("Modelo: ");
+            string modelo = Console.ReadLine();
+            Console.Write("Descrição do produto: ");
+            string descricao = Console.ReadLine();
+            Console.Write("Plataforma Afiliado: ");
+            string plataforma = Console.ReadLine();
+            Console.Write("Qual categoria pertence? ");
+            string categoria = Console.ReadLine();
+            Console.Write("Link para compra: ");
+            string link = Console.ReadLine();
+            Console.Write("Link da imagem do produto: ");
+            string imagemurl = Console.ReadLine();
+
+            Produto produtos = new Produto(id, nome, marca, modelo, descricao, plataforma, categoria, link, imagemurl);
+            listaDeProdutos.Add(produtos);
+
+            Console.WriteLine("Deseja adicionar um novo produto? (S/N)");
+            continuar = Console.ReadLine();
+        }
+        Console.WriteLine("-----------------------");
+        Console.WriteLine("\nPRODUTOS CADASTRADOS:");
+        foreach(var produto in listaDeProdutos) {
+            Console.WriteLine();
+            produto.Exibir();
+        }
+
     }
 
 }
