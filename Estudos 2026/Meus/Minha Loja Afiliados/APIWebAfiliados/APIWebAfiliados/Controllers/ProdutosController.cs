cd374c037895a1d3dd5a58686012722030b1f686
Bruno Paschoal <programming.paschoal@gmail.com>
2026-05-11 20:52:48 -0300

    teste

---
+++ Estudos 2026/Meus/Minha Loja Afiliados/APIWebAfiliados/APIWebAfiliados/Controllers/ProdutosController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace APIWebAfiliados.Controllers {
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProdutosController : ControllerBase {
+        private static List<Produto> listaDeProdutos = new List<Produto>();
+
+        [HttpGet]
+        public IEnumerable<Produto> Get() {
+            return listaDeProdutos;
+        }
+    }
+}
