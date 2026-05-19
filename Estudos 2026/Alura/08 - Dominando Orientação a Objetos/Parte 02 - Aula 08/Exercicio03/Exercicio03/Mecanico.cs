fb20800dc84dc39ee43cdb66bd4cad7b1d4602f3
Bruno Paschoal <programming.paschoal@gmail.com>
2026-05-19 20:57:48 -0300

    Exercício 03 - Aula 08 Parte 02: Criar um programa Program.cs e simular o funcionamento do programa.

---
+++ Estudos 2026/Alura/08 - Dominando Orientação a Objetos/Parte 02 - Aula 08/Exercicio03/Exercicio03/Mecanico.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio03 {
+    internal class Mecanico {
+        public string Nome { get; set; }
+        public string Especialidade { get; set; }
+
+        public Mecanico(string nome, string especialidade) {
+            Nome = nome;
+            Especialidade = especialidade;
+        }
+    }
+}
---
+++ Estudos 2026/Alura/08 - Dominando Orientação a Objetos/Parte 02 - Aula 08/Exercicio03/Exercicio03/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Criar um programa Program.cs e simular o funcionamento do programa. */
+
+using Exercicio03;
+using System;
+
+internal class Program {
+
+    public static void Main(string[] args) {
+        Veiculo meuCarro = new Veiculo("Chevrolet", "Cruze", 2020, "ABC1234");
+        Cliente cliente = new Cliente("Carlos", "987654321");
+        Mecanico mecanico = new Mecanico("Mário", "Mecânica Geral");
+        Oficina oficina = new Oficina();
+
+        // Agendar e realizar um serviço na oficina
+        oficina.AgendarServico(meuCarro, cliente, mecanico, "2023-12-18");
+        oficina.RealizarServico(meuCarro, mecanico);
+    }
+}
