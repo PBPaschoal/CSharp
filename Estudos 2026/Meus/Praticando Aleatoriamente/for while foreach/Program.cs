67a67ff465f29f4398a582e4fd52c12301c6e21d
Bruno Paschoal <programming.paschoal@gmail.com>
2026-07-16 21:21:20 -0300

    Praticando for, while e foreach

---
+++ Estudos 2026/Meus/Praticando Aleatoriamente/for while foreach/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Write("Qual tabuada deseja calcular? ");
+int valor = int.Parse(Console.ReadLine());
+int resultado = 0;
+for(int i = 0; i <= 10; i++) {
+    resultado = valor * i;
+    Console.WriteLine($"{valor} * {i} = {resultado}");
+}
+
+string senhaOriginal = "aBc@123";
+int tentativas = 5;
+while(tentativas != 0) {
+    Console.WriteLine("Informe a senha: ");
+    string senhaDigitada = Console.ReadLine();
+    if(senhaDigitada != senhaOriginal) {
+        Console.WriteLine("Senha invalida!");
+        tentativas--;
+        if(tentativas == 0) {
+            Console.WriteLine("Limite de tentativa de acesso atingido.");
+        }
+    } else {
+        tentativas = 0;
+        Console.WriteLine("Acesso permitido!");
+    }
+}
+
+string[] nomes = {"Bruno", "Bruna", "Kabeça", "Lola", "Pepe Antonio", "Jhenna", "Belinha", "Fred", "Pandora", "Simon"};
+foreach(string nome in nomes) {
+    Console.WriteLine(nome);
+}
