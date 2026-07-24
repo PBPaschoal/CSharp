e820936fc68c7c5db869c4fb84ceec41668cb0b6
Bruno Paschoal <programming.paschoal@gmail.com>
2026-07-23 22:07:07 -0300

    Exercícios 1011 até 1017 beecrowd Nova conta

---
+++ Estudos 2026/Beecrowd/Conta Nova/Exercicio1013/Exercicio1013/Program.cs
@@ -0,0 +1,36 @@
+﻿/* Faça um programa que leia três valores e apresente o maior dos três valores lidos seguido da mensagem “eh o maior”. Utilize a fórmula:
+
+
+
+Obs.: a fórmula apenas calcula o maior entre os dois primeiros (a e b). Um segundo passo, portanto é necessário para chegar no resultado esperado.
+
+Entrada
+O arquivo de entrada contém três valores inteiros.
+
+Saída
+Imprima o maior dos três valores seguido por um espaço e a mensagem "eh o maior".
+
+Exemplos de Entrada	Exemplos de Saída
+7 14 106
+
+106 eh o maior
+
+217 14 6
+
+217 eh o maior */
+
+using System;
+
+class Program {
+    static void Main(string[] args) {
+        string[] valores = Console.ReadLine().Split(' ');
+        int a = int.Parse(valores[0]);
+        int b = int.Parse(valores[1]);
+        int c = int.Parse(valores[2]);
+
+        int maiorAB = (a + b + Math.Abs(a - b)) / 2;
+        int maiorFinal = (maiorAB + c + Math.Abs(maiorAB - c)) / 2;
+
+        Console.WriteLine($"{maiorFinal} eh o maior");
+    }
+}
---
+++ Estudos 2026/Beecrowd/Conta Nova/Exercicio1015/Exercicio1015/Program.cs
@@ -0,0 +1,44 @@
+﻿/* Leia os quatro valores correspondentes aos eixos x e y de dois pontos quaisquer no plano, p1(x1,y1) e p2(x2,y2) e calcule a distância entre eles, mostrando 4 casas decimais, segundo a fórmula:
+
+Distancia =
+
+Entrada
+O arquivo de entrada contém duas linhas de dados. A primeira linha contém dois valores de ponto flutuante: x1 y1 e a segunda linha contém dois valores de ponto flutuante x2 y2.
+
+Saída
+Calcule e imprima o valor da distância segundo a fórmula fornecida, considerando 4 casas decimais.
+
+Exemplo de Entrada	Exemplo de Saída
+1.0 7.0
+5.0 9.0
+
+4.4721
+
+-2.5 0.4
+12.1 7.3
+
+16.1484
+
+2.5 -0.4
+-12.2 7.0
+
+16.4575 */
+
+using System;
+using System.Globalization;
+
+class Program {
+    static void Main(string[] args) {
+        string[] p1 = Console.ReadLine().Split(' ');
+        double x1 = double.Parse(p1[0], CultureInfo.InvariantCulture);
+        double y1 = double.Parse(p1[1], CultureInfo.InvariantCulture);
+        string[] p2 = Console.ReadLine().Split(' ');
+        double x2 = double.Parse(p2[0], CultureInfo.InvariantCulture);
+        double y2 = double.Parse(p2[1], CultureInfo.InvariantCulture);
+
+        double distancia = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+
+        Console.WriteLine($"{distancia.ToString("F4", CultureInfo.InvariantCulture)}");
+
+    }
+}
