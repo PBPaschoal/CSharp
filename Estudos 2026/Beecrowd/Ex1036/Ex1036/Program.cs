d8897b427ed6279916cda91f19e8108cfe070b1f
Bruno Paschoal <programming.paschoal@gmail.com>
2026-08-23 18:57:29 -0300

    Exercícios conta antiga: 1036 - Fórmula de Bhaskara

---
+++ Estudos 2026/Beecrowd/Ex1036/Ex1036/Program.cs
@@ -0,0 +1,42 @@
+﻿/* Leia 3 valores de ponto flutuante e efetue o cálculo das raízes da equação de Bhaskara. Se não for possível calcular as raízes, mostre a mensagem correspondente “Impossivel calcular”, caso haja uma divisão por 0 ou raiz de numero negativo.
+
+Entrada
+Leia três valores de ponto flutuante (double) A, B e C.
+
+Saída
+Se não houver possibilidade de calcular as raízes, apresente a mensagem "Impossivel calcular". Caso contrário, imprima o resultado das raízes com 5 dígitos após o ponto, com uma mensagem correspondente conforme exemplo abaixo. Imprima sempre o final de linha após cada mensagem
+
+Exemplos de Entrada	        Exemplos de Saída
+10.0 20.1 5.1               R1 = -0.29788
+                            R2 = -1.71212
+
+0.0 20.0 5.0                Impossivel calcular
+
+10.3 203.0 5.0              R1 = -0.02466
+                            R2 = -19.68408
+
+10.0 3.0 5.0                Impossivel calcular */
+
+using System;
+using System.Globalization;
+
+class Program {
+    static void Main(string[] args) {
+        string[] vetor = Console.ReadLine().Split(' ');
+        double A = double.Parse(vetor[0], CultureInfo.InvariantCulture);
+        double B = double.Parse(vetor[1], CultureInfo.InvariantCulture);
+        double C = double.Parse(vetor[2], CultureInfo.InvariantCulture);
+
+        double delta = Math.Pow(B, 2.0) - (4.0 * A * C);
+
+        if (A == 0 || delta < 0) {
+            Console.WriteLine("Impossivel calcular");
+        }
+        else {
+            double r1 = (-B + Math.Sqrt(delta)) / (2.0 * A);
+            double r2 = (-B - Math.Sqrt(delta)) / (2.0 * A);
+            Console.WriteLine($"R1 = {r1.ToString("F5", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"R2 = {r2.ToString("F5", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
